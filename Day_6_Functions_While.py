3b4436665d1f6b2ff32c12ac181d1405d34063a1
Omer <omer@omer-linux.cable.rcn.com>
2022-12-22 22:37:03 -0500

    Added new codes in Day6 files

--- Day_6_Functions_While.py
+++ Day_6_Functions_While.py
@@ -1,103 +1,57 @@
-# # Functions
+# Functions
 
-# # Built-in Functions
-# # print() Name of a function followed by a set of parantheses
-# # len()
+# Built-in Functions
+# print() Name of a function followed by a set of parantheses
+# len()
 
-# # Create a Function
-# # def my_function():
-# #    Do this
-# # my_function() Calling function
-
-
-# # While Loops
-
-# # While something_is_true:
-   # # Do something repeatedly
+# Create a Function
+# def my_function():
+#    Do this
+# my_function() Calling function
 
 
-# # Hangman
+# While Loops
 
-import random
+# While something_is_true:
+   # Do something repeatedly
 
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
+# Example Simple Function
 
-end_of_game = False
-word_list = ["penguin", "nato", "christmas", "democracy", "malware", "board"]
-chosen_word = random.choice(word_list)
-word_length = len(chosen_word)
-lives = 6
+def greet():
+   print("Hello")
+   print("How do you do?")
+   print("Isn't the weather nice today?")
 
-display = []
-for _ in range(word_length):
-    display += "_"
+greet()
 
-while not end_of_game:
-    guess = input("Guess a letter: ").lower()
+# Example 
 
-if guess not in chosen_word:
-        lives -= 1
-        if lives == 0:
-            end_of_game = True
-            print("You lose.")
-print(f"{' '.join(display)}")
+def greet_with_name(name):  # Function that allows for Input
+   print(f"Hello {name}")
+   print(f"How do you do {name}?")
+   print("Isn't the weather nice today?")
 
-if "_" not in display:
-        end_of_game = True
-        print("You win.")
-print(stages[lives])
+greet_with_name("Axel")
+
+# Example
+
+def greet_with(name, surname):  # Functions with more than 1 input
+   print(name, surname)
+
+greet_with("Jason", "Bourne")
+greet_with(name="Jason", surname="Bourne")
+greet_with(surname="Bourne", name="Jason")
+
+
+# Paint Area Calculator
+
+import math
+
+def paint_calc(height, width, cover):
+   number_cans = math.ceil((height * width) / cover)
+   print(f"You will need {number_cans} cans of paint")
+
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
